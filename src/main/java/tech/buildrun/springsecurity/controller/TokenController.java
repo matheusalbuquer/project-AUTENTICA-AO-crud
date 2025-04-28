ea9bdd8d3a3ebe28040f49fc93364d0e37dc4cc9
matheus albuquer <mj898926@gmail.com>
2025-04-27 22:26:00 -0300

    updates

--- src/main/java/tech/buildrun/springsecurity/controller/TokenController.java
+++ src/main/java/tech/buildrun/springsecurity/controller/TokenController.java
@@ -1,20 +1,34 @@
 package tech.buildrun.springsecurity.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import tech.buildrun.springsecurity.controller.dto.LoginRequest;
+import tech.buildrun.springsecurity.controller.dto.LoginResponse;
+import tech.buildrun.springsecurity.reposioty.UserRepository;
 
 @RestController
 public class TokenController {
 
     private final JwtDecoder jwtDecoder;
+    private final UserRepository userRepository;
 
-    public TokenController(JwtDecoder jwtDecoder){
+    public TokenController(JwtDecoder jwtDecoder,UserRepository userRepository){
         this.jwtDecoder = jwtDecoder;
+        this.userRepository = userRepository;
     }
 
 
     @PostMapping("/login")
-    public ResponseEntity<UserD>
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
+        var user = userRepository.findByUsername(loginRequest.username());
+
+        if(user.isEmpty()){
+            throw new BadCredentialsException("user or password is inválid");
+        }
+    }
+
 }
